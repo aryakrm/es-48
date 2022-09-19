d05de3ffac629095294dcc3ced007c841003cbf8
Arya Karimi <devaryakrm@gmal.com>
2022-09-19 10:41:48 +0300

    codes added

---
+++ exercise.js
@@ -0,0 +1,5 @@
+const students = [
+  { id: "1", name: "Arya", surname: "Karimi", age: 23 },
+  { id: "2", name: "Jack", surname: "Pawl", age: 26 },
+  { id: "3", name: "Joe", surname: "Cole", age: 19 },
+];
